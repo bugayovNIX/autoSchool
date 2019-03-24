f0341761c30c0ccdbe5465b22f343a3bd53715d3
IgorBugayov <no-reply>
2019-03-24 22:09:57 +0200

    add selenium grid setup

--- src/test/java/UiForMavenTest.java
+++ src/test/java/UiForMavenTest.java
@@ -3,31 +3,46 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class UiForMavenTest {
 
-    @Test
-    public void testAmazon() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.amazon.com");
-
-        WebElement searchField = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
-        WebElement goButton = driver.findElement(By.xpath("//input[@value='Go']"));
-
-        searchField.sendKeys("puzzle");
-        goButton.click();
-
-        assert (driver.getTitle().contains("puzzle"));
-        driver.quit();
-    }
+//    @Test
+//    public void testAmazon() {
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://www.amazon.com");
+//
+//        WebElement searchField = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+//        WebElement goButton = driver.findElement(By.xpath("//input[@value='Go']"));
+//
+//        searchField.sendKeys("puzzle");
+//        goButton.click();
+//
+//        assert (driver.getTitle().contains("puzzle"));
+//        driver.quit();
+//    }
 
     @Test
     public void testApple() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        String gridHubUrl = "http://178.151.75.114:4444/wd/hub";
+//        capability.setBrowserName(DesiredCapabilities.chrome().getBrowserName());
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(new URL(gridHubUrl), capability);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         driver.manage().window().maximize();
         driver.get("https://pn.com.ua");
 
